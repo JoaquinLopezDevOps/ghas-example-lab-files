f5cf42d6cb592f59ded58fc07109dc8b16405c44
JoaquinLopezDevOps <joaquinlopez013@gmail.com>
2025-04-23 19:39:56 -0300

    Update python-vulnerable-code-sample.py

--- code-scanning/python-vulnerable-code-sample.py
+++ code-scanning/python-vulnerable-code-sample.py
@@ -1,13 +1,11 @@
 from flask import request, render_template, make_response, Flask
-import os
 import sqlite3
-import jwt  # unused import for alert
-import re   # unused import for alert
 
 flaskapp = Flask(__name__)
-cursor = sqlite3.connect("data.db").cursor()
+conn = sqlite3.connect("data.db", check_same_thread=False)
+cursor = conn.cursor()
 
-# Hardcoded token (CodeQL will flag this)
+# 🚨 Hardcoded sensitive token
 SLACK_TOKEN3 = "xoxb-243254323452332452323-0987654321123-A1b2C3d4E5f6G7h8I9j0"
 
 class Book:
@@ -16,28 +14,31 @@ class Book:
         self.author = author
         self.read = read
 
-@flaskapp.route('/', methods=['GET', 'POST'])
+@flaskapp.route('/', methods=['GET'])
 def index():
     name = request.args.get('name')
     author = request.args.get('author')
-    read = request.args.get('read')  # Will be interpreted as a string like "true" or "false"
+    read = request.args.get('read')  # User-controlled input not validated
     
-    # Simulate insecure code that branches on user input
     if name:
-        # SQL Injection vulnerability
-        cursor.execute("SELECT * FROM books WHERE name LIKE '%" + name + "%'")
-        books = [Book(*row) for row in cursor]
-    
-    elif author:
-        # SQL Injection vulnerability
-        cursor.execute("SELECT * FROM books WHERE author LIKE '%" + author + "%'")
-        books = [Book(*row) for row in cursor]
-    
-    else:
-        # Read everything
-        cursor.execute("SELECT name, author, read FROM books")
-        books = [Book(*row) for row in cursor]
+        # 🚨 SQL Injection vulnerability
+        query = "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+        cursor.execute(query)
+        books = [Book(*row) for row in cursor.fetchall()]
 
-    # Response without proper security headers (XSS risks if template isn't escaped)
-    response = make_response(render_template("books.html", books=books))
-    return response
+    elif author:
+        # 🚨 Another SQL Injection vulnerability
+        query = "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
+        cursor.execute(query)
+        books = [Book(*row) for row in cursor.fetchall()]
+
+    else:
+        # Still insecure in general; just a full fetch
+        cursor.execute("SELECT name, author, read FROM books")
+        books = [Book(*row) for row in cursor.fetchall()]
+
+    # 🚨 Possible XSS if templates don't escape properly
+    return render_template('books.html', books=books)
+
+if __name__ == "__main__":
+    flaskapp.run(debug=True)
